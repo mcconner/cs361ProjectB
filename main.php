3fd739cdb01fbb3b0efcede865f2736bab2e04ce
Rachael <rachael.mcconnell@comcast.net>
2015-08-07 19:59:19 -0700

    Testing new API for fiscal requirements

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-Type: text/html');
+header("Access-Control-Allow-Origin: *");
 // session_save_path('/nfs/stak/students/l/liangt/php_sessions');
 // session_start();
 ?>
@@ -15,6 +16,9 @@ header('Content-Type: text/html');
 	<!-- Bootstrap Core CSS -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
 	<!-- Custom CSS -->
 	<style>
 		body {
@@ -75,8 +79,9 @@ header('Content-Type: text/html');
             </h4>
         </div>
         <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-            <div class="panel-body">
-                
+            <div class="panel-body table-responsive">
+                <p>This section will assess your personality traits and attributes to give you a list of careers that are a 
+                    good fit for you.</p>
                 <div id="assessmentJSON"></div>
 				  <div id="slideDeck"></div>
 				  <div id="careersJSON"></div>​
@@ -94,9 +99,29 @@ header('Content-Type: text/html');
         </div>
         <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
             <div class="panel-body" id="fiscalRequirements">
+                <p>This section will help you to figure out the fiscal requirements that you will need to follow in order to have
+                the lifestyle you desire.</p> 
+
+
+                <!--<div class="input-group input-group-sm">
+                  <span class="input-group-addon" id="sizing-addon3">Enter your ideal location (City Name)</span>
+                  <input type="text" class="form-control" placeholder="Location" aria-describedby="sizing-addon3">
+                </div>
+                <div class="input-group input-group-sm">
+                  <input type="submit" class="form-control" placeholder="Location" aria-describedby="sizing-addon3" onclick="getFiscalRequirements()">
+                </div>-->
+
+                <form onsubmit="getFiscalRequirements()">
+                    <label>Enter your ideal location:</label><input type="text" placeholder="Location" id="Location"></input>
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
+
+                <div id="fiscal"></div>
 
             </div>
         </div>
+
     </div>
     <div class="panel panel-default" style="border: 2px solid #33A6CC; border-radius: 5px;">
         <div class="panel-heading" role="tab" id="headingTwo">
@@ -128,6 +153,62 @@ header('Content-Type: text/html');
     </div>
 </div>
 
+
+
+<script>
+
+//function getFiscalRequirements() {
+//    alert("Form submitted");
+//    ajaxCall();
+    //make ajax call
+    
+    //$.get("http://www.numbeo.com/api/city_prices?api_key=vr5x2c8nzqofyv&query=San+Francisco").done(function (data) {
+     //   console.log(data);
+    //});
+    
+
+//    trHTML = '<p>Fiscal data</p>';
+//    var fiscalData = document.getElementById("fiscal");
+//    fiscalData.innerHTML = trHTML;
+//}
+
+
+/*function getFiscalRequirements() {
+    alert("In function");
+    $.ajax({
+        url: 'http://www.numbeo.com/api/city_prices?api_key=vr5x2c8nzqofyv&query=San+Francisco',
+        type: 'GET',
+        dataType: 'json',
+        headers: 'Access-Control-Allow-Origin: *',
+        success: function(data){
+            alert("Success!");
+            console.log(data);
+        },
+        error: function (){
+            alert("Error");
+        }
+
+    });
+    }*/
+
+    function getFiscalRequirements() {
+       $.ajax({
+        headers: { "Accept": "application/json"},
+            type: 'GET',
+            url: 'http://www.numbeo.com/api/city_prices?api_key=vr5x2c8nzqofyv&query=San+Francisco',
+            crossDomain: true,
+            beforeSend: function(xhr){
+                xhr.withCredentials = true;
+          },
+            success: function(data, textStatus, request){
+                console.log(data);
+            }
+        }); 
+    }
+    
+    
+
+</script>
 
 </body>
 </html>
